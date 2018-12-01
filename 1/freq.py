9f1e00e1d7e709da69d1f63bf1866ad83e63c8c5
Niklas Krolovitsch <niklassk8@gmail.com>
2018-12-01 21:49:58 +0100

    Add solution for day 1

---
+++ 1/freq.py
@@ -0,0 +1,37 @@
+"""
+Advent of code day 1 solution.
+
+Given a list of integers, calculates the end frequency.
+"""
+def calculate_part1(steps: list) -> int():
+    """
+    Calculates the final frequency by accounting for each frequency step in steps.
+
+    steps = list(int)
+    """
+    return sum(steps)
+
+
+def calculate_part2(steps: list) -> int():
+    """
+    Returns the first frequency step that is reached twice.
+    """
+    freq_history = set()
+    freq = 0
+    not_found = True
+    while not_found:
+        for step in steps:
+            freq_history.add(freq)
+            freq += step
+            if freq in freq_history:
+                return freq
+
+
+
+if __name__ == '__main__':
+    steps = []
+    with open('input.txt', 'r') as instructions:
+        for line in instructions:
+            steps.append(int(line))
+
+    print(calculate_part2(steps))
---
+++ 1/test_freq.py
@@ -0,0 +1,71 @@
+"""
+Advent of Code day 1 solution.
+
+Test suite for solution of day 1.
+"""
+import unittest
+
+import freq
+
+
+def test_input_1_part1():
+    expected = 3
+    stimuli = [1, 1, 1]
+
+    result = freq.calculate_part1(stimuli)
+
+    assert expected == result
+
+
+def test_input_2_part1():
+    expected = 0
+    stimuli = [1, 1, -2]
+
+    result = freq.calculate_part1(stimuli)
+
+    assert expected == result
+
+
+def test_input_3_part1():
+    expected = -6
+    stimuli = [-1, -2, -3]
+
+    result = freq.calculate_part1(stimuli)
+
+    assert expected == result
+
+
+def test_input_1_part2():
+    expected = 0
+    stimuli = [1, -1]
+
+    result = freq.calculate_part2(stimuli)
+
+    assert expected == result
+
+
+def test_input_2_part2():
+    expected = 10
+    stimuli = [3, 3, 4, -2, -4]
+
+    result = freq.calculate_part2(stimuli)
+
+    assert expected == result
+
+
+def test_input_3_part2():
+    expected = 5
+    stimuli = [-6, 3, 8, 5, -6]
+
+    result = freq.calculate_part2(stimuli)
+
+    assert expected == result
+
+
+def test_input_4_part2():
+    expected = 14
+    stimuli = [7, 7, -2, -7, -4]
+
+    result = freq.calculate_part2(stimuli)
+
+    assert expected == result
